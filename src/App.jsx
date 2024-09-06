14bfd840fa03d0cd99472f08a88e561542a8e1d1
UTakuto <taku.0307.ue@icloud.com>
2024-09-07 01:44:41 +0900

    mainViewBox 作成 css調整

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,33 @@ export default function App() {
         <>
             <Header />
             <div className="main">
-                <h1>Home</h1>
-                <p>uemori takuto</p>
+                <section className="mainViewBox">
+                    <div className="mainView"></div>
+                    <div className="mainViewText">
+                        <h2>welcome my portfolio</h2>
+                    </div>
+                </section>
 
-                <div className="about">
+                <section className="about">
                     <div className="bakCenter"></div>
                     <div className="myPicture"></div>
-                    <section className="aboutTextContent">
+                    <div className="aboutTextContent">
                         <h2>About</h2>
                         <h3>
                             上森 拓翔 <span>uemori takuto</span>
                         </h3>
                         <p>
-                            ECCコンピュータ専門学校/Webデザインコース
+                            ECCコンピュータ専門学校 Webデザインコース
                             26年卒業予定
                             <br />
-                            2005年3月7日/兵庫県出身/趣味：音楽鑑賞,写真撮影
+                            2005年3月7日 / 兵庫県出身 / 趣味：音楽鑑賞,写真撮影
                             <br />
-                            エンジニアを目指して日々精進しています。現在はReact.jsを勉強しており
-                            <br />
-                            今後は他のライブラリやフレームワークを触れるようにしたいと考えております。
+                            エンジニアを目指して日々精進しています。
                         </p>
 
                         <Button />
-                    </section>
-                </div>
+                    </div>
+                </section>
             </div>
             <Footer />
         </>
