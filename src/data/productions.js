bed65efe056f624024ea924a1b99d29e5e4bf888
UTakuto <taku.0307.ue@icloud.com>
2025-01-23 18:10:58 +0900

    productionInfoを外部データからインポートし、ハードコーディングされた情報を削除

---
+++ src/data/productions.js
@@ -0,0 +1,16 @@
+export const productionInfo = [
+    {
+        img: "../src/assets/RelaxCoffee.png",
+        date: "2023.04~2023.07",
+        title: "Relax coffee",
+        grade: "1年 前期作品・個人",
+        type: "design/code",
+    },
+    {
+        img: "../src/assets/PartTimeManager.jpg",
+        date: "2023.10~2024.02",
+        title: "バイト マネージャー",
+        grade: "1年 後期作品・個人",
+        type: "design/code",
+    },
+];
--- src/layouts/Productions.jsx
+++ src/layouts/Productions.jsx
@@ -1,24 +1,8 @@
 import ProductionItem from "../components/production/ProductionItem";
+import { productionInfo } from "../data/productions.js";
 import "../styles/Productions.css";
 
 export default function Productions() {
-    const productionInfo = [
-        {
-            img: "../src/assets/RelaxCoffee.png",
-            date: "2023.04~2023.07",
-            title: "Relax coffee",
-            grade: "1年 前期作品・個人",
-            type: "design/code",
-        },
-        {
-            img: "../src/assets/PartTimeManager.jpg",
-            date: "2023.10~2024.02",
-            title: "バイト マネージャー",
-            grade: "1年 後期作品・個人",
-            type: "design/code",
-        },
-    ];
-
     return (
         <ul className="productionWrap">
             <li className="productionList">
