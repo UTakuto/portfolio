c1c507d239b99a6b7615bab0e26273e9131ec0db
UTakuto <taku.0307.ue@icloud.com>
2025-01-23 18:11:07 +0900

    PhotoPageのヘッダーを更新し、スタイルを追加。新しいCSSファイルを作成し、レイアウトを改善。

--- src/pages/PhotoPage.jsx
+++ src/pages/PhotoPage.jsx
@@ -1,10 +1,14 @@
 import PhotoGrids from "../components/photo/PhotoGrids";
+import "../styles/photo.css";
 
 export default function PhotoPage() {
     console.log("PhotoPage");
     return (
         <div>
-            <h1>Photo Gallery</h1>
+            <div className="photoHeader">
+                <h1>My Photo Gallery</h1>
+                <p>無断転載・利用はご遠慮ください</p>
+            </div>
             <PhotoGrids />
         </div>
     );
