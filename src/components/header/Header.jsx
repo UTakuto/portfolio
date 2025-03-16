0da3ae1159cf41ae2fc0458f283f3828797e443b
UTakuto <taku.0307.ue@icloud.com>
2025-03-17 03:14:27 +0900

    ヘッダーコンポーネントのリンクにデータ属性を追加し、構造を整理

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,31 +2,37 @@ import "./header.css";
 
 export default function Header() {
     return (
-        <>
-            <header>
-                <figure className="logo">
-                    <a href="#">
-                        <img src="/portfolioLogo.svg" alt="portfolioLogo" />
-                    </a>
-                </figure>
+        <header>
+            <figure className="logo">
+                <a href="#" data-text="Portfolio">
+                    <img src="/portfolioLogo.svg" alt="portfolioLogo" />
+                </a>
+            </figure>
 
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#">Works</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#" data-text="Home">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" data-text="About">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" data-text="Works">
+                            Works
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" data-text="Contact">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 }
