0859ea6c28749f9bc387ed7ee9028fd0936193fc
UTakuto <taku.0307.ue@icloud.com>
2025-03-17 03:14:31 +0900

    新しいプロダクション情報を追加し、作品タイトルや日付、グレードを更新

--- src/data/productions.js
+++ src/data/productions.js
@@ -1,5 +1,19 @@
 export const productionInfo = [
     {
+        img: "../src/assets/monosirihaunter.png",
+        date: "2024.10~2025.02",
+        title: "ものしりハンター",
+        grade: "2年 後期作品・チーム",
+        type: "code",
+    },
+    {
+        img: "../src/assets/RelaxCoffee.png",
+        date: "2024.05~2024.07",
+        title: "歴てく",
+        grade: "2年 前期作品・チーム・企業連携",
+        type: "code",
+    },
+    {
         img: "../src/assets/RelaxCoffee.png",
         date: "2023.04~2023.07",
         title: "Relax coffee",
