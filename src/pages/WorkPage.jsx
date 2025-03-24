0ece4cb003f48b7a9cb5345d1070a4a095fff34d
UTakuto <taku.0307.ue@icloud.com>
2025-03-24 14:22:43 +0900

    WorkPageコンポーネントを作成し、Productionsレイアウトを追加

---
+++ src/pages/WorkPage.jsx
@@ -0,0 +1,11 @@
+import Productions from "../layouts/Productions";
+
+export default function WorkPage() {
+    return (
+        <section className="works">
+            <h2>Works</h2>
+
+            <Productions />
+        </section>
+    );
+}
