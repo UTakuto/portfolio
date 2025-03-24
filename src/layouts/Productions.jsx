cd25374a14396ca5928e49a20c2d077e1eb7b213
UTakuto <taku.0307.ue@icloud.com>
2025-03-24 14:22:24 +0900

    ProductionsコンポーネントにWorkFilterを追加し、カテゴリフィルタリング機能を実装

--- src/layouts/Productions.jsx
+++ src/layouts/Productions.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import ProductionItem from "../components/production/ProductionItem";
+import WorkFilter from "../components/work/WorkFilter";
 import { productionInfo } from "../data/productions.js";
 import "../styles/Productions.css";
 
 export default function Productions() {
+    const [selectedCategory, setSelectedCategory] = useState("all");
+
+    const filteredProductions = productionInfo.filter((info) =>
+        selectedCategory === "all" ? true : info.category === selectedCategory
+    );
+
     return (
-        <ul className="productionWrap">
-            <li className="productionList">
-                {productionInfo.map((info, index) => (
-                    <ProductionItem key={index} info={info} />
-                ))}
-            </li>
-        </ul>
+        <div>
+            <WorkFilter
+                selectedCategory={selectedCategory}
+                onCategoryChange={setSelectedCategory}
+            />
+            <ul className="productionWrap">
+                <li className="productionList">
+                    {filteredProductions.map((info, index) => (
+                        <ProductionItem key={index} info={info} />
+                    ))}
+                </li>
+            </ul>
+        </div>
     );
 }
