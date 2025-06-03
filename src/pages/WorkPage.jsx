6f88750291476237eb1370f4a3848cf84e76e276
UTakuto <taku.0307.ue@icloud.com>
2025-06-03 15:21:59 +0900

    h2要素にIDを追加して、セクションの識別を可能にしました

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -3,7 +3,7 @@ import Productions from "../layouts/Productions";
 export default function WorkPage() {
     return (
         <section className="works">
-            <h2>Works</h2>
+            <h2 id="works-section">Works</h2>
 
             <Productions />
         </section>
