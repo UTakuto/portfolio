396379c1ce1f874f9cca7d0e29467756ab066997
UTakuto <taku.0307.ue@icloud.com>
2025-06-17 19:05:27 +0900

    ButtonコンポーネントにonClickプロパティを追加し、ボタンのクリックイベントを処理できるように変更

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./button.css";
 
-export default function Button({ text = "more", link = "/photo" }) {
+export default function Button({ text = "more", link = "/photo", onClick }) {
     return (
         <div className="buttonContainer">
-            <Link to={link} className="button">
-                {text}
-            </Link>
+            {onClick ? (
+                <button onClick={onClick} className="button">
+                    {text}
+                </button>
+            ) : (
+                <Link to={link} className="button">
+                    {text}
+                </Link>
+            )}
         </div>
     );
 }
@@ -15,4 +21,5 @@ export default function Button({ text = "more", link = "/photo" }) {
 Button.propTypes = {
     text: PropTypes.string,
     link: PropTypes.string,
+    onClick: PropTypes.func,
 };
